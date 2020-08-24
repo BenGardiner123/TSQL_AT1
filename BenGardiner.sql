2878b385d142e03d669eec8a141132933c0c3d13
BenGardiner123 <61527372+BenGardiner123@users.noreply.github.com>
2020-08-24 10:17:01 +1000

    Add customer complete - ex.tested

--- BenGardiner.sql
+++ BenGardiner.sql
@@ -58,4 +58,50 @@ CREATE SEQUENCE SALE_SEQ;
 
 GO
 
+-- END PROVIDED DDL ----- BEGIN TSQL WORK ---------------------
 
+IF OBJECT_ID('ADD_CUSTOMER') IS NOT NULL
+DROP PROCEDURE ADD_CUSTOMER;
+GO
+
+CREATE PROCEDURE ADD_CUSTOMER @PCUSTID INT, @PCUSTNAME NVARCHAR(100) AS
+
+BEGIN
+    BEGIN TRY
+
+        IF @PCUSTID < 1 OR @PCUSTID > 499
+            THROW 50020, 'Customer ID out of range', 1
+
+        INSERT INTO CUSTOMER (CUSTID, CUSTNAME, SALES_YTD, STATUS) 
+        VALUES (@PCUSTID, @PCUSTNAME, 0, 'OK');
+
+    END TRY
+    BEGIN CATCH
+        if ERROR_NUMBER() = 2627
+            THROW 50010, 'Duplicate customer ID', 1
+        ELSE IF ERROR_NUMBER() = 50020
+            THROW
+        ELSE
+            BEGIN
+                DECLARE @ERRORMESSAGE NVARCHAR(MAX) = ERROR_MESSAGE();
+                THROW 50000, @ERRORMESSAGE, 1
+            END; 
+    END CATCH;
+
+END;
+
+GO
+
+-- execute and test here - - - - -- - - - -
+
+EXEC ADD_CUSTOMER @pcustid = 1, @pcustname = 'testdude2';
+EXEC ADD_CUSTOMER @pcustid = 2, @pcustname = 'testdude3';
+EXEC ADD_CUSTOMER @pcustid = 3, @pcustname = 'testdude5';
+
+-- EXEC ADD_CUSTOMER @pcustid = 500, @pcustname = 'testdude3';
+
+--EXEC ADD_CUSTOMER @pcustid = 1, @pcustname = 'testdude3'; 
+
+select * from customer;
+
+GO
